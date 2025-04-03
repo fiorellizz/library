cdc2d200a11e344a0ad64098044bf6dd3810e0e4
fiorellizz <fiorelligui@gmail.com>
2025-04-03 13:44:28 -0300

    feat: criar autores e categorias, chaves unicas

--- main2.py
+++ main2.py
@@ -39,17 +39,64 @@ def listar_usuarios(lista_usuarios):
         print(f"E-mail: {usuario['email']}")
         print(f"Tipo: {usuario['tipo'].capitalize()}")
 
-def cadastrar_livro(lista_livros, lenLista):
+def cadastrar_livro(lista_livros):
+    id_unico = len(lista_livros) + 1
     livro = {}
-    livro['id'] = lenLista + 1
+    livro['id'] = id_unico
     livro['titulo'] = input('Digite o nome do livro: ')
     livro['autor'] = input('Digite o nome do autor: ')
-    livro['anoPublicacao'] = int(input('Digite o ano de publicação: '))
-    livro['isbn'] = input('Digite a ISBN: ')
+    livro['anoPublicacao'] = input('Digite o ano de publicação: ')
+    
+    while True:
+        isbn = input('Digite a ISBN: ')
+        if any(livro.get('isbn') == isbn for livro in lista_livros):
+            print('Erro: Já existe um livro com esta ISBN. Por favor, insira um valor único.')
+        else:
+            livro['isbn'] = isbn
+            break
+    
     livro['categoria'] = input('Digite o nome da categoria: ')
     lista_livros.append(livro)
     print('\nLivro cadastrado com sucesso!')
+    
     return lista_livros
+
+def cadastrar_autor(lista_autores):
+    id_unico = len(lista_autores) + 1
+    autor = {}
+    autor['id'] = id_unico
+    autor['nome'] = input('Digite o nome do autor: ')
+    
+    while True:
+        orcid = input('Digite o ORCID do autor: ')
+        if any(autor.get('orcid') == orcid for autor in lista_autores):
+            print('Erro: Já existe um autor com este ORCID. Por favor, insira um valor único.')
+        else:
+            autor['orcid'] = orcid
+            break
+    
+    lista_autores.append(autor)
+    print('\nAutor cadastrado com sucesso!')
+    
+    return lista_autores
+
+def cadastrar_categoria(lista_categorias):
+    id_unico = len(lista_categorias) + 1
+    categoria = {}
+    categoria['id'] = id_unico
+    
+    while True:
+        nome = input('Digite o nome da categoria: ')
+        # Verifica se já existe uma categoria com este nome
+        if any(cat.get('nome') == nome for cat in lista_categorias):
+            print('Erro: Já existe uma categoria com este nome. Por favor, insira um nome único.')
+        else:
+            categoria['nome'] = nome
+            break
+    
+    lista_categorias.append(categoria)
+    print('\nCategoria cadastrado com sucesso!')
+    return lista_categorias
 
 def listar_livros(lista_livros):
     if not lista_livros:
@@ -97,7 +144,9 @@ def buscar_livros(lista_livros):
 
 def main():
     lista_livros = []
-    lista_usuarios = []  
+    lista_usuarios = []
+    lista_autores = []
+    lista_categorias = []
     emails_cadastrados = set()
     
     # ler arquivos csv caso existam e preencher a lista
@@ -109,12 +158,14 @@ def main():
         print("3 - Buscar livros")
         print("4 - Cadastrar usuário")
         print("5 - Listar usuários")
+        print("6 - Cadastrar novo autor")
+        print("7 - Cadastrar nova categoria")
         print("0 - Sair")
         
         option = int(input("Digite a sua escolha: "))
         
         if option == 1:
-            lista_livros = cadastrar_livro(lista_livros, len(lista_livros))
+            lista_livros = cadastrar_livro(lista_livros)
         elif option == 2:
             listar_livros(lista_livros)
         elif option == 3:
@@ -123,9 +174,15 @@ def main():
             lista_usuarios = cadastrar_usuario(emails_cadastrados, lista_usuarios)
         elif option == 5:
             listar_usuarios(lista_usuarios)
+        elif option == 6:
+            lista_autores = cadastrar_autor(lista_autores)
+        elif option == 7:
+            lista_categorias = cadastrar_categoria(lista_categorias)
         elif option == 0:
             print(lista_livros)
             print(lista_usuarios)
+            print(lista_autores)
+            print(lista_categorias)
             print(emails_cadastrados)
             # salvar nos arquivos csv as listas
             print("Saindo do sistema...")
